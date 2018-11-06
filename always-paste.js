4bcd8bf57ab249975f282225d4b741965f113488
Robin Berjon <robin@berjon.com>
2018-11-05 22:33:23 -0500

    outline of menu-based approach

--- always-paste.js
+++ always-paste.js
@@ -1,73 +1,31 @@
 
-let hasPaste = null;
+// XXX switch this to background script
+let id = 'always-paste'
+  , options = {
+      id,
+      title:    'Paste Anyway!',
+      contexts: ['edtiable'],
+      icons:    {
+        16: './clippy-octicon.svg',
+      },
+      enabled:   false,
+    }
+;
 
-window.addEventListener(
-  'focus',
-  (evt) => {
-    let { target } = evt;
-    if (!target || target.nodeType !== 1 || target.localName !== 'input' || (target.type !== 'text' && target.type !== 'password')) return;
-    // XXX
-    //  - show a button next to the input that can be activated to paste
-    hasPaste = attachButton(target);
-    document.body.appendChild(hasPaste);
-  },
-  true
-);
-
-window.addEventListener(
-  'blur',
-  (evt) => {
-    console.log(`blur`, hasPaste);
-    if (!hasPaste) return;
-    let { target } = evt;
-    if (!target || target.nodeType !== 1 || target.localName !== 'input' || (target.type !== 'text' && target.type !== 'password')) return;
-    // XXX
-    //  - remove the button next to force paste
-    hasPaste.parentNode.removeChild(hasPaste);
-    hasPaste = null;
-  },
-  true
-);
-
-window.addEventListener(
-  'click',
-  (evt) => {
-    console.log(`click`, hasPaste);
-    if (!hasPaste) return;
-    let { target } = evt;
-    if (hasPaste === target) console.log(`MATCH!`);
-    console.log('clicked...');
-  },
-  false
-);
-
-function handlePaste () {
-  console.log(`click`);
-  // XXX
-  //  - write clipboard straight to the value
-}
-
-function attachButton (target) {
-  if (!target) return;
-  let but = document.createElement('button')
-    , { top, left, height, width } = target.getBoundingClientRect()
-  ;
-  let right = left + width - height;
-  setStyle(but, {
-    position:   'absolute',
-    width:      `${height}px`,
-    height:     `${height}px`,
-    top:        `${top}px`,
-    left:       `${right}px`,
-    margin:       0,
-    padding:      0,
-    background:   '#00f',
-    border:       'none',
-  });
-  // but.onclick = handlePaste;
-  return but;
-}
-
-function setStyle (el, props) {
-  Object.keys(props).forEach(k => el.style[k] = props[k]);
-}
+browser.contextMenus.create(options);
+browser.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === id) {
+      let el = browser.menus.getTargetElement(info.targetElementId);
+      if (!el) return;
+      navigator
+        .clipboard
+        .readText()
+        .then(txt => el.value = txt)
+      ;
+    }
+});
+browser.contextMenus.onShown.addListener(async function (info) {
+  let txt = await navigator.clipboard.readText();
+  browser.menus.update(id, { enabled: !!txt });
+  browser.menus.refresh();
+});
